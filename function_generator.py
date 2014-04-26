08aaee9f035acd119fc69472a9bbb8b82d09d60a
aeftimia <alexeftimiades@gmail.com>
2014-04-26 09:21:10 -0400

    eliminated rng variable

--- function_generator.py
+++ function_generator.py
@@ -3,22 +3,21 @@ from PyQt4.QtCore import *
 import subprocess
 import sys
 
-rng = 200000
-
 def change_freq(value):
     global p
     p.terminate()
     p.wait()
-    p = play(value / 10)
+    p = play(value)
 
 def play(value):
+    print "%dHz" % value
     return subprocess.Popen(["play", "-r", "192000", "-n", "synth", "sine", str(value)])
 
 if __name__ == "__main__":
     app=QApplication(sys.argv)
     slider=QSlider(Qt.Horizontal)
-    slider.setRange(1, rng)
-    p = play(120)
+    slider.setRange(20, 2000)
+    p = play(100)
     QObject.connect(slider,SIGNAL("valueChanged(int)"), change_freq)
     slider.show()
     #Start the evnt loop
